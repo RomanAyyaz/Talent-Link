b3de1cdb2fc5fdb1a3d59b30e737936d362cee32
Roman Ayyaz <romanayaz7@gmail.com>
2024-07-31 10:48:42 +0500

    Course List Section Completed

--- client/src/Components/User/LandingPage/CoursesList/CourseListImages.jsx
+++ client/src/Components/User/LandingPage/CoursesList/CourseListImages.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function CourseListImages({data}) {
   return (
-    <div className='relative my-4'>
-        <img src={data.Image} alt="" />
+    <div className='relative my-4 cursor-pointer'>
+        <img src={data.Image} alt="" className={ `lg:w-64 lg:h-64`} />
         <div className='absolute bg-white p-4 w-48 bottom-6 left-1/2 transform -translate-x-1/2 text-center text-md font-medium'>
             {data.courselist}
         </div>
--- client/src/Components/User/LandingPage/CoursesList/CoursesList.jsx
+++ client/src/Components/User/LandingPage/CoursesList/CoursesList.jsx
@@ -8,35 +8,38 @@ function CoursesList() {
     let cousrseListData = [
         {
             Image:ImageOne,
-            courselist:'DEVELOPMENT'
+            courselist:'DEVELOPMENT',
         },
         {
             Image:ImageTwo,
-            courselist:'BUSINESS'
+            courselist:'BUSINESS',
         },
         {
             Image:ImageThree,
-            courselist:'ENTERPRENEURSHIP'
+            courselist:'ENTERPRENEURSHIP',
         }
     ]
   return (
-    <div>
-        <div className='mt-14 text-start px-5'>
-            <h1 className='font-bold text-4xl hidden md:block'>Find Courses on almost any topic</h1>
+    <div className='lg:flex lg:mt-6'>
+        <div className='mt-14 text-start px-5 lg:px-8 lg:w-2/5 lg:py-28'>
+            <h1 className='font-bold text-4xl hidden md:block lg:w-[400px] lg:leading-[65px] lg:text-6xl'>Find Courses on almost any topic</h1>
             <h1 className='font-semibold text-4xl md:hidden'>Find Courses on </h1>
             <h1 className='font-semibold text-4xl md:hidden'> almost any topic</h1>
             <h1 className='my-6 text-2xl font-medium md:hidden mt-14'>Build your library for your career and personal growth</h1>
             <h1 className='hidden md:block text-2xl font-normal mt-14'>Build your library for your career</h1>
             <h1 className='hidden md:block text-2xl font-normal' >and personal growth</h1>
-            <p className='text-sm mt-8'>VIEW COURSES <BsArrowRight size={15} className='inline-block'/></p>
+            <p className='text-sm mt-8 cursor-pointer'>VIEW COURSES <BsArrowRight size={15} className='inline-block'/></p>
             <div className='bg-gray-500 h-[1px] mt-1 w-[120px]'></div>
         </div>
-        <div className='mt-14 px-5'>
+        <div className='mt-14 px-5 lg:flex lg:gap-3 lg:flex-wrap lg:justify-center lg:w-3/5'>
             {cousrseListData.map((values,index)=>{
                 return(
                     <CourseListImages data = {values} key = {index}/>
                 )
             })}
+            <div className='m-auto lg:m-0 relative w-52 h-64 bg-HeroButtonOne cursor-pointer'>
+                <p className='text-white p-4 w-32 absolute bottom-6 left-12 text-center text-2xl font-semibold'>BROWSE OTHER COURSES</p>
+            </div>
         </div>
     </div>
   )
