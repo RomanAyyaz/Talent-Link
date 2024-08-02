45c0b92623a735f45b7a4c916a08932c1496ee97
AreebaZaheer <areeba9645@gmail.com>
2024-08-03 03:46:13 +0500

    Your commit message here

--- client/src/Components/User/LandingPage/WebsiteInfo/WebsiteInfoImages.jsx
+++ client/src/Components/User/LandingPage/WebsiteInfo/WebsiteInfoImages.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WebsiteInfoImages({ data }) {
     return (
         <div className='relative w-full lg:w-full lg:h-full hidden lg:block'>
-            <img src={data.Image} alt=""  className=' lg:h-96 w-full h-auto object-cover' style={{ clipPath: 'polygon(0% 3%, 100% 70%, 100% 100%, 0 100%)' }} />
+            <img src={data.Image} alt=""  className=' lg:h-96 w-full h-auto object-cover' style={{ clipPath: 'polygon(0 0, 75% 0, 100% 100%, 0% 100%)' }} />
             <div >
                 {data.websiteinfo}
             </div>
